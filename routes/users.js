219f8726365122462d46735f341d0e395fb413b6
jatin1793 <104287949+jatin1793@users.noreply.github.com>
2023-04-11 20:49:35 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://0.0.0.0:27017/testdb");
+mongoose.connect("mongodb+srv://jatin-wakodikar:Jatin@4196@mera-cluster.karqf3f.mongodb.net/?retryWrites=true&w=majority");
 
 const userSchema = mongoose.Schema({
   username: String,
